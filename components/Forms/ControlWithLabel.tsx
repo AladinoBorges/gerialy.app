2cf4e3e2c8a4697459946e8874b9c2016f83bf6f
¨Aladino <aladino@headhunters.digital>
2024-08-13 10:28:57 -0300

    feat[components]: forms - control with label
    - implements generic reusable form control with label component.

---
+++ components/Forms/ControlWithLabel.tsx
@@ -0,0 +1,31 @@
+import { BaseComponentPropTypes } from '@/types/generic';
+import { FormControl, FormErrorMessage, FormHelperText, FormLabel } from '@chakra-ui/react';
+
+export function FormControlWithLabel({
+  label,
+  children,
+  helperText,
+  isRequired,
+  errorMessage,
+}: PropTypes) {
+  return (
+    <FormControl isRequired={isRequired}>
+      <FormLabel>{label}</FormLabel>
+
+      {children}
+
+      {!!helperText?.trim() && !errorMessage?.trim() ? (
+        <FormHelperText>{helperText}</FormHelperText>
+      ) : null}
+
+      {!!errorMessage?.trim() ? <FormErrorMessage>{errorMessage}</FormErrorMessage> : null}
+    </FormControl>
+  );
+}
+
+interface PropTypes extends BaseComponentPropTypes {
+  label: string;
+  helperText?: string;
+  isRequired?: boolean;
+  errorMessage?: string;
+}

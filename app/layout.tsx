9a489a7de7ed826698ec2f355ed662ed638ae7e3
¨Aladino <aladino@headhunters.digital>
2024-08-13 07:29:31 -0300

    chore[app]: ends base configuration of app project.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Providers } from './providers';
 export const metadata: Metadata = {
   title: 'ger.ia | gestão eficiente de vagas e candidaturas',
   description: 'gerir vagas e candidaturas com inteligência artificial',
+  icons: { icon: { url: '/beta logo.png', href: '/beta logo.png' } },
 };
 
 export default function RootLayout({

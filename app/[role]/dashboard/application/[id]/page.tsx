34df2e7735d3b02884ca0598d3fc8f0e47314510
¨Aladino <aladino@headhunters.digital>
2024-08-19 12:44:11 -0300

    feat[pages]: role based dashboard application by id
    - implements dependent application by id query algorithm.

--- app/[role]/dashboard/application/[id]/page.tsx
+++ app/[role]/dashboard/application/[id]/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { ApplicationType } from '@/types/application';
 import { IDType } from '@/types/generic';
 import { Flex } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 import gerapi from '../../../../../services/geriapi';
 import { session } from '../../../../../services/session';
@@ -9,13 +11,24 @@ import { session } from '../../../../../services/session';
 export default function AnalysisResumePage({ params: { id } }: PropTypes) {
   const [sessionToken, setSessionToken] = useState<string | null>(null);
 
-  const getApplication = async (id: string | number, token: string) => {
-    const API_URL = `${process.env.NEXT_PUBLIC_API_ULR}/application`;
+  const getApplication = async () => {
+    const API_URL = `applications/${id}`;
 
-    const application = await gerapi.get(API_URL, token);
+    const application: ApplicationType = await gerapi.get(API_URL, sessionToken as string);
 
     return application;
   };
+
+  const {
+    data: application,
+    isError,
+    isPending,
+    isSuccess,
+  } = useQuery({
+    queryKey: ['applicationByID'],
+    queryFn: getApplication,
+    enabled: !!sessionToken?.trim(),
+  });
 
   useEffect(() => {
     (async () => {

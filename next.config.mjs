e6d9b8792b46e65f6408be3cf0f735284f295d8d
aladino_borges <eualadino@gmail.com>
2025-10-02 16:00:40 -0300

    chore: replaces react with preact on webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, output: "standalone" };
+
+const handleWebpackConfiguration = (configuration, { dev, isServer }) => {
+  if (!isServer) {
+    Object.assign(configuration.resolve.alias, {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat"
+    });
+  }
+
+  return configuration;
+};
+
+const nextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  webpack: handleWebpackConfiguration
+};
 
 export default nextConfig;

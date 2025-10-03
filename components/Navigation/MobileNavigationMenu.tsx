249eb4d2a8bc06e36edd7f243f22070558d094b3
aladino_borges <eualadino@gmail.com>
2025-10-03 20:30:40 -0300

    feat[app]: implements dynamic navigation menu

---
+++ components/Navigation/MobileNavigationMenu.tsx
@@ -0,0 +1,114 @@
+import { session } from '@/services/session';
+import {
+  Button,
+  HStack,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuGroup,
+  MenuItem,
+  MenuList,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import Link from 'next/link';
+import { Fragment } from 'react';
+import { IconType } from 'react-icons';
+import {
+  HiMiniCpuChip,
+  HiOutlineBuildingStorefront,
+  HiOutlineCodeBracket,
+  HiOutlineHomeModern,
+  HiOutlinePuzzlePiece,
+  HiOutlineScale,
+  HiOutlineUserGroup,
+} from 'react-icons/hi2';
+import publicLinks from '../../mocks/publicLinks.json';
+
+interface MobileNavigationMenuProps {
+  startLogoutProcess: VoidFunction;
+}
+
+export default function MobileNavigationMenu({ startLogoutProcess }: MobileNavigationMenuProps) {
+  const { data } = useQuery({
+    queryKey: ['userToken'],
+    queryFn: () => session.getCookie('user'),
+  });
+
+  const isLogged = Boolean(data);
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
+  const icons: { [key: string]: IconType } = {
+    homepage: HiOutlineHomeModern,
+    marketplace: HiOutlineBuildingStorefront,
+    projects: HiOutlineCodeBracket,
+    'about us': HiOutlineUserGroup,
+    legal: HiOutlineScale,
+    mAIn: HiOutlinePuzzlePiece,
+  };
+
+  return isMobile ? (
+    <Menu isLazy placement='auto'>
+      <MenuButton as={IconButton} aria-label='Options' icon={<HiMiniCpuChip />} variant='outline' />
+      <MenuList>
+        <MenuGroup title='gerialy'>
+          {publicLinks.map(({ label, href }, index) => {
+            const Icon = icons[label];
+
+            return (
+              <MenuItem
+                icon={<Icon />}
+                key={index}
+                as={Link}
+                href={href}
+                isDisabled={!['homepage', 'mAIn'].includes(label)}
+              >
+                {label}
+              </MenuItem>
+            );
+          })}
+        </MenuGroup>
+
+        {isLogged ? (
+          <Fragment>
+            <MenuDivider />
+
+            <MenuGroup title='user'>
+              <MenuItem icon={<HiMiniCpuChip />} onClick={startLogoutProcess}>
+                logout
+              </MenuItem>
+            </MenuGroup>
+          </Fragment>
+        ) : null}
+      </MenuList>
+    </Menu>
+  ) : (
+    <HStack spacing={0}>
+      {publicLinks.map(({ label, href }, index) => {
+        const Icon = icons[label];
+
+        return (
+          <Button
+            as={Link}
+            href={href}
+            key={index}
+            leftIcon={<Icon />}
+            variant='ghost'
+            size='sm'
+            isDisabled={!['homepage', 'mAIn'].includes(label)}
+          >
+            <Text size='md'>{label}</Text>
+          </Button>
+        );
+      })}
+
+      {isLogged ? (
+        <Button variant='outline' onClick={startLogoutProcess}>
+          logout
+        </Button>
+      ) : null}
+    </HStack>
+  );
+}
